f98b236f92640ceb0fac79e911c423d05c75be84
UniBen <u1765774@unimail.hud.ac.uk>
2019-03-04 12:35:46 +0000

    Add more types

--- config/laravelgraphqlable.php
+++ config/laravelgraphqlable.php
@@ -1,15 +1,27 @@
 <?php
 
-use GraphQL\Type\Definition;
+use GraphQL\Type\Definition\BooleanType;
+use GraphQL\Type\Definition\FloatType;
+use GraphQL\Type\Definition\IntType;
+use GraphQL\Type\Definition\StringType;
 
 return [
-    // Default
+    // Defaults
     'default'   => StringType::class,
+
+    // Integers
+    'int'       => IntType::class,
+    'float'     => FloatType::class,
+    'double'    => IntType::class,
 
     // Strings
     'text'      => StringType::class,
     'char'      => StringType::class,
     'varchar'   => StringType::class,
 
+    // Booleans
+    'tinyint'   => BooleanType::class,
+
     // Times
+    'timestamp' => StringType::class,
 ];
